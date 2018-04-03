1dc8fcd246ebe3392171eb04e6da64a36b967a38
Mathias Hannus <mathias.hannus@nfs.fi>
2018-04-03 22:14:01 +0300

    Uudet tekstit päivitetty sivuille

--- src/MainPage.js
+++ src/MainPage.js
@@ -10,16 +10,8 @@ class MainPage extends Component {
       <div className="mainPage">
         <div className="filler-div">&nbsp;</div>
         <div className="contentBox">
-          <h1>PORVOON KUNTOAJOT 6.8. 2017 KLO 10.00</h1>
+          <h1>PORVOON KUNTOAJOT LAUNTAINA 18.8. 2017 KLO 10.00</h1>
 
-          <p style={{fontWeight: "bold"}}>
-          Ennätyksellisen osanottajajoukon keräävässä Porvoon Kuntoajossa huomenna 6.8 klo 10.00 vielä tilaa jälki-ilmoittautuneille.<br/>Kaikki paikalle tulevat mahtuvat mukaan! Tervetuloa!
-          </p>
-          <p>
-          Huominen sääennuste lupaa lähes optimaalista ajosäätä, pilvipoutaa ja lämpötila tasainen 17 asetta. Lähdön hetkellä pienen pieni mahdollisuus, että tulee pieni virkistävä sade, mutta sen jälkeen pelkkää poutaa.
-          </p>
-
-          <p style={{fontWeight: "bold"}}>Lista ennakkoon ilmoittautuneista löydät <Link to="/lahtolista">tästä</Link></p>
           Ajajilta suurta kiitosta saaneissa Porvoon Kuntoajoissa on kaksi eri matkaa:
           <ul>
             <li>Pidempi reitti on 110 kilometriä ja luonteeltaan mäkinen ja vaativa.</li>
@@ -38,14 +30,7 @@ class MainPage extends Component {
 
           <div id="ilmoittautuminen">
             <h2>Ilmoittautuminen</h2>
-            <p>Ilmoittaudu sähköpostiosoitteeseen: <a href="mailto:akilles.porvoonkuntoajot@gmail.com">akilles.porvoonkuntoajot@gmail.com</a>.<br/>
-            Sähköpostissa täytyy mainita ajajan nimi, puhelinnumero sekä ajettava matka.<br/>
-            Vastausviestinä saat kuittauksen ilmoittautumisen saapumisesta.
-            </p>
-            <p>Osallistumismaksu maksetaan välittömästi ilmoittautumisen jälkeen
-            Akilleen Pyöräilyjaoston tilille:  Aktia  FI4140501950009588.<br/>
-            Maksun viestikentässä täytyy näkyä ilmoittautuneen ajajan tai
-            joukkueen/yhteyshenkilön nimi ja ajettava matka.
+            <p> Ilmoittautuminen avataan piakkoin
             </p>
           </div>
           <div id="hinnat">
